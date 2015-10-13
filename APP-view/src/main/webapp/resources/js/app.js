d69ce526b56e8b20aa68f5d2c09f64a9724a8734
JorgeLuis <lescanojorgeluis@gmail.com>
2015-10-13 02:19:10 -0300

    login con facebook, falta arreglar un error con la url principal

--- APP-view/src/main/webapp/resources/js/app.js
+++ APP-view/src/main/webapp/resources/js/app.js
@@ -4,8 +4,8 @@ var app =angular.module('app',['ngRoute','ngResource','restApiModule','ngMap']);
 app.config(function($routeProvider, $httpProvider) {
       $routeProvider.
    	//Home
-//      	when('/', {controller:loginController, templateUrl:"app/login"}).
-      	when('/', {controller:homeController, templateUrl:"app/home"}).      	
+      	when('/', {controller:loginController, templateUrl:"app/login"}).
+      	when('/home', {controller:homeController, templateUrl:"app/home"}).      	
       	//Administracion
       	//Partidos
       	when('/createMatch', {controller:createMatchController, templateUrl:'app/matchTemplate'}).//Consulta
